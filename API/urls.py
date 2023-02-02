d09366e1cc7554c93468f3a906c52b59a075f284
FadhliFathoni <fadhlifathoni62@gmail.com>
2023-02-02 20:09:57 +0700

    Add Manual

--- API/urls.py
+++ API/urls.py
@@ -1,3 +1,4 @@
+#Router API
 from django.urls import path
 from . import views
 
--- Karyawan/models.py
+++ Karyawan/models.py
@@ -1,13 +1,6 @@
+#Inisialisasi Tabel
 from django.db import models
 from mongoengine import Document, fields
-
-# Create your models here.
-
-# class Karyawan(Document):
-#     nama = fields.StringField()
-#     password = fields.StringField()
-#     email = fields.EmailField()
-#     alamat = fields.StringField()
 
 class Karyawan(models.Model):
     nama = models.CharField(max_length=20, unique=True)
